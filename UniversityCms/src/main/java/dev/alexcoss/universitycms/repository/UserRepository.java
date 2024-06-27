51a760fc8916742da0c14d301692c7706f745196
alexcoss <kozachenko.aleksander@gmail.com>
2024-06-27 13:02:13 +0200

    small fix

--- UniversityCms/src/main/java/dev/alexcoss/universitycms/repository/UserRepository.java
+++ UniversityCms/src/main/java/dev/alexcoss/universitycms/repository/UserRepository.java
@@ -12,5 +12,6 @@ import java.util.Set;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
 
+    @Query("SELECT u.username FROM User u")
     Set<String> findAllUsernames();
 }
